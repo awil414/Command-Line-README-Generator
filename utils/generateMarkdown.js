f43bf54601f2ab1fe4027be5d857cb57ce4727a0
Amber Wilson <amberwilson@MacBook-Pro.local>
2022-10-03 10:26:49 -0500

    adding the rest of the generateMarkdown for README function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,31 +1,31 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-const License = true;
+//const License = true;
 
 function renderLicenseBadge(license) {
-    switch (new License().renderLicenseBadge()) {
-      case 0:
-        license = "MIT";
-        break;
-      case 1:
-        license = "APACHE 2.0";
-        break;
-      case 2:
-        license = "BSD 3";
-        break;
-      case 3:
-        license = "GPL 3.0";
-        break;
-      case 4:
-        license = "Mozilla";
-        break;
-      case 5:
-        license = "None";
-        break;
-      case 6:
-        license = "";
-    }
-    return
+  switch (new License().renderLicenseBadge()) {
+    case 0:
+      license = "MIT";
+      break;
+    case 1:
+      license = "APACHE 2.0";
+      break;
+    case 2:
+      license = "BSD 3";
+      break;
+    case 3:
+      license = "GPL 3.0";
+      break;
+    case 4:
+      license = "Mozilla";
+      break;
+    case 5:
+      license = "None";
+      break;
+    case 6:
+      license = "";
+  }
+  return
 }
 
 // TODO: Create a function that returns the license link
@@ -74,12 +74,26 @@ To install necessary dependencies, run the following command:
 ${data.installation}
 \`\`\`
 
+## Usage
+${data.usage}
 
+${renderLicenseSection(data.license)}
 
+## Contributing
+${data.contributing}
 
+## Tests
 
+To run tests, run the following command:
 
-`;
+\`\`\`
+${data.test}
+\`\`\`
+
+## Questions
+
+If you have any questions about the repo, constact me at ${data.email}. 
+You can find more of my work at [${data.github}](https://github.com/${data.github}/).`;
 }
 
 module.exports = generateMarkdown;
