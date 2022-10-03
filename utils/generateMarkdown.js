7b9f39d4e5feb820539c5e654f6187d62b0eed22
Amber Wilson <amberwilson@MacBook-Pro.local>
2022-10-02 20:37:16 -0500

    adding function to render license section

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -25,15 +25,32 @@ function renderLicenseBadge(license) {
       case 6:
         license = "";
     }
+    return
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== "None") {
+    return (
+      `\n "https://choosealicense.com/licenses/" \n`
+    )
+  }
+  return ""
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== "None") {
+    return (
+      `## License
+      
+  This project is licensed under the ${license} license.`
+    )
+  }
+  return ""
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
